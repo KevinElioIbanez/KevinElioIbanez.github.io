785d99c236bdb4cbbd08c6a5b9ea281a34a0fbd6
Kevin Ibañez <kevinelioibanez@gmail.com>
2023-01-04 14:03:28 -0300

    agregando switch-menu

--- js/script.js
+++ js/script.js
@@ -1,30 +1,11 @@
-/* Menu de navegacion */
+/* Menu bars */
 
-document.querySelector(".navegador__menu").addEventListener("click", animacionMenu);
+const bars = document.getElementById("menu");
+const menu = document.getElementById("sidebar");
 
-let linea1 = document.querySelector(".line1__bars-menu");
-let linea2 = document.querySelector(".line2__bars-menu");
-let linea3 = document.querySelector(".line3__bars-menu");
-let menu__nav = document.querySelector(".navegador__nav");
+bars.addEventListener("click", () => {
+  menu.classList.add("sidebar--show")
+  bars.classList.remove("bx-menu")
+  bars.classList.add("bx-x")
+})
 
-function animacionMenu(){
-    linea1.classList.toggle("activeline1__bars-menu");
-    linea2.classList.toggle("activeline2__bars-menu");
-    linea3.classList.toggle("activeline3__bars-menu");
-    menu__nav.classList.toggle("active__nav")
-}
-
-/* Carrusel del Header */
-
-var elem = document.querySelector('.main-carousel');
-var flkty = new Flickity( elem, {
-  // options
-  cellAlign: 'left',
-  contain: true
-});
-
-// element argument can be a selector string
-//   for an individual element
-var flkty = new Flickity( '.main-carousel', {
-  // options
-});
