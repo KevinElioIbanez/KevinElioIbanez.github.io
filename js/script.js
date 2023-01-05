667acad7f5d113c90ba44e5391c8780c5075eba8
Kevin Ibañez <kevinelioibanez@gmail.com>
2023-01-05 03:37:54 -0300

    Navegador terminado

--- js/script.js
+++ js/script.js
@@ -1,7 +1,35 @@
+/* Menu Mobile */
+
 const menu = document.getElementById("menu");
 const sidebar = document.getElementById("sideBar");
 
 menu.addEventListener("click", () => {
     menu.classList.toggle("bx-x");
     sidebar.classList.toggle("sideBar--show");
+})
+
+/* Menu Escritorio */
+
+const navItems = document.querySelectorAll("nav .nav-item")
+const toggle = document.getElementById("toggle");
+
+toggle.addEventListener("click", () => {
+    if(sidebar.className == "sideBar") {
+        sidebar.classList.add("open")
+    }
+    else {
+        sidebar.classList.remove("open")
+    }
+})
+
+navItems.forEach(item => {
+    item.addEventListener("click", () => {
+        
+        navItems.forEach(navItems => {
+            navItems.classList.remove("active")
+        })
+
+        item.classList.add("active")
+
+    })
 })
