13cc79b00369016794a399246e32f00f44be3301
Kevin Ibañez <kevinelioibanez@gmail.com>
2023-01-07 02:29:55 -0300

    seccion de perfil terminado sin efecto de dezplazamiento por el costado

--- js/script.js
+++ js/script.js
@@ -15,6 +15,7 @@ menu.addEventListener("click", () => {
 const toggle = document.getElementById("toggle");
 const sections = document.querySelectorAll("section");
 
+
 toggle.addEventListener("click", () => {
 
     sections.forEach(section => {
@@ -34,6 +35,14 @@ toggle.addEventListener("click", () => {
         sidebar.classList.remove("open")
     }
 
+    /* sidebar.addEventListener("resize", () => {
+
+        sectionProfile.addEventListener("click", () => {
+            alert("ASDASDSAD")
+        })
+
+    }) */
+
 })
 
 
@@ -43,21 +52,21 @@ const navItems = document.querySelectorAll('nav .nav-item');
 const enlaces = document.querySelectorAll('a');
 
 function handleClick(e) {
-  e.preventDefault();
-  const href = this.getAttribute('href');
-  const seccion = document.querySelector(href);
-  seccion.scrollIntoView({
-    behavior: 'smooth',
-    block: 'start',
-    inline: 'nearest'
-  });
+    e.preventDefault();
+    const href = this.getAttribute('href');
+    const seccion = document.querySelector(href);
+    seccion.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start',
+        inline: 'nearest'
+    });
 }
 
 navItems.forEach(item => {
-  item.addEventListener('click', function() {
-    navItems.forEach(navItem => navItem.classList.remove('active'));
-    this.classList.add('active');
-  });
+    item.addEventListener('click', function () {
+        navItems.forEach(navItem => navItem.classList.remove('active'));
+        this.classList.add('active');
+    });
 });
 
 enlaces.forEach(enlace => enlace.addEventListener('click', handleClick));
@@ -80,3 +89,48 @@ const sectionShow = new IntersectionObserver(vista => {
 
 
 hiddenElement.forEach((e) => sectionShow.observe(e))
+
+/* Cambio de ancho cuando el sidebar se agranda */
+
+const aboutMi = document.querySelector(".about__mi h3"),
+      aboutMiP = document.querySelector(".about__mi p"),
+      aboutMih4 = document.querySelectorAll(".about__details h4"),
+      aboutMiUl = document.querySelector(".datos-intereses ul"),
+      aboutMiLi = document.querySelectorAll(".datos-intereses ul li")
+
+const observer = new ResizeObserver(() => {
+
+    if (sidebar.offsetWidth <= 360 && sidebar.offsetWidth > 120 && window.innerWidth >= 768 && window.innerWidth < 900) {
+
+        aboutMi.classList.add("titulo")
+        aboutMiP.classList.add("parrafo")
+        aboutMih4.forEach( h4 => {
+            h4.classList.add("detalles")
+        })
+        aboutMiUl.classList.add("intereses")
+        aboutMiLi.forEach(li => {
+            li.classList.add("interes")
+        })
+
+    } else {
+
+        aboutMi.classList.remove("titulo");
+        aboutMiP.classList.remove("parrafo")
+        aboutMih4.forEach( h4 => {
+            h4.classList.remove("detalles")
+        })
+        aboutMiUl.classList.remove("intereses")
+        aboutMiLi.forEach(li => {
+            li.classList.remove("interes")
+        })
+
+    }
+
+});
+
+observer.observe(sidebar);
+
+
+
+
+
